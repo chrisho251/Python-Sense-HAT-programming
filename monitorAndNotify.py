5c69329eb6e2ee54d39d89b9ce4d58a5f68df529
chrisho251 <hoduyhoang250196@gmail.com>
2020-08-02 14:13:45 +0700

    Update monitorAndNotify.py

---
+++ monitorAndNotify.py
@@ -0,0 +1,7 @@
+import MySQLdb
+from environment import temperature
+
+#Create a connection between raspberry and database
+connection = MySQLdb.connect("localhost", "pi", "raspberry", "raspberrydb")
+
+with connection.cursor() as cursor:
